e26740c20f2af91b4b2e270c0c4819f350d646bb
kriti1807 <83088348+kriti1807@users.noreply.github.com>
2021-09-14 12:09:17 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+print("CALCULATOR")
+print("1. Addition\n2. Substraction\n3. Multiplication\n4. Division")
+p=int(input("Choose an option:"))
+x=int(input("Enter Number 1="))
+y=int(input("Enter Number 2="))
+
+if p==1:
+    print("The summation of the above the two numbers is", x+y)
+elif p==2:
+    print("The difference of the above two numbers is", x-y)
+elif p==3:
+    print("The product of the above the two numbers is", x*y)
+else:
+    print("The summation of the above the two numbers is", x//y)
